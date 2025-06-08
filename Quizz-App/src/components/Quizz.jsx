8463add459a34fe7040171b5afcf02d5e95484d3
attrenate <nikolozghomidzevol100@gmail.com>
2025-06-09 01:42:02 +0400

    change

--- Quizz-App/src/components/Quizz.jsx
+++ Quizz-App/src/components/Quizz.jsx
@@ -6,26 +6,24 @@ import EndScreen from "./EndScreen";
 
 
 function Quizz(){
-    const {gameState, setGameState} = useContext(QuizContext);
+    const { gameState, setGameState, score, setScore } = useContext(QuizContext);
     const [currQuestion, setCurrQuestion] = useState(0);
     const [optionChosen, setOptionChosen] = useState("");
 
     const nextQuestion = () => {
-        if(Questions[currQuestion].answer == optionChosen){
+        if(Questions[currQuestion].answer === optionChosen){
             setScore(score + 1);
         }
-        console.log(score);
         setCurrQuestion(currQuestion + 1);
-
-
     };
 
     const finishQuiz = () => {
-         if(Questions[currQuestion].answer == optionChosen){
+         if(Questions[currQuestion].answer === optionChosen){
             setScore(score + 1);
         }
-        setGameState("gameState");
+        setGameState("EndScreen");
     };
+
     return(
         <div className="quiz">
             <h1>{Questions[currQuestion].prompt}</h1>
@@ -35,14 +33,14 @@ function Quizz(){
                 <button onClick={() => setOptionChosen("C")}>{Questions[currQuestion].optionC}</button>
                 <button onClick={() => setOptionChosen("D")}>{Questions[currQuestion].optionD}</button>
             </div>
-            {currQuestion == Questions.length - 1 ? (
+            {currQuestion === Questions.length - 1 ? (
                 <button onClick={finishQuiz}>Finish Quiz</button>
             ) : (
                 <button onClick={nextQuestion}>Next Question</button>
-            )};
-            <button onClick={nextQuestion}>Next Question</button>
+            )}
         </div>
     );
 }
+
 
 export default Quizz
