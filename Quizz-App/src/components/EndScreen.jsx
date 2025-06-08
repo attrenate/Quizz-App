dac6a289a9fc11fc543a22f1777dc135cd950a28
attrenate <nikolozghomidzevol100@gmail.com>
2025-06-09 02:03:34 +0400

    App finished

--- Quizz-App/src/components/EndScreen.jsx
+++ Quizz-App/src/components/EndScreen.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { QuizContext } from "../Helpers/Contexts";
+import "../App.css";
+import { Questions } from "../Helpers/QuestionBank";
+
 
 function EndScreen(){
+    const { score, setScore, setGameState } = useContext(QuizContext);
+    const restartQuiz = () => {
+        setScore(0);
+        setGameState("menu");
+    };
     return(
-        <div>End Screeen</div>
+        <div className="EndScreen">
+            <h1>Quiz Finished</h1>
+            <h3>
+                {score} / {Questions.length}
+            </h3>
+            <button onClick={restartQuiz}>Restart Quiz</button>
+        </div>
     );
 }
 
